f8a59ba74e042732f6077d1fcbb09d66f6191af1
LukaFlores <luka.flores@mail.com>
2023-04-03 16:58:23 -0700

    leetcode(typescript): 28

---
+++ leetcode/typescript/28-find-the-index-of-the-first-occurrence-in-a-string/build.sh
@@ -0,0 +1,16 @@
+
+#!/bin/sh
+
+set -xe
+
+if [ -z $1 ]; then
+	echo Add a filename to command: ./build.sh filename
+
+	else
+    ts-node 28-find-the-index-of-the-first-occurrence-in-a-string.ts
+
+    time ( ts-node 28-find-the-index-of-the-first-occurrence-in-a-string.ts) >&1 1>/dev/null 
+
+
+fi
+         
